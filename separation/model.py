4627475aceb5a333c44c96fef04158a925a27c15
jljl1337 <lckjack123@gmail.com>
2024-01-19 17:00:16 +0800

    Add lightning module and update naming

--- separation/model.py
+++ separation/model.py
@@ -1,15 +1,17 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import pytorch_lightning as pl
+from torchmetrics.aggregation import MeanMetric
 
-def down_layer(in_channels, out_channels, kernel_size=5, stride=2, padding=2):
+def _down_layer(in_channels, out_channels, kernel_size=5, stride=2, padding=2):
     return nn.Sequential(
         nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
         nn.BatchNorm2d(out_channels),
         nn.LeakyReLU(0.2, inplace=True),
     )
 
-def up_layer(in_channels, out_channels, kernel_size=5, stride=2, padding=1, dropout=False):
+def _up_layer(in_channels, out_channels, kernel_size=5, stride=2, padding=1, dropout=False):
     layers = nn.Sequential(
         nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding),
         nn.BatchNorm2d(out_channels),
@@ -25,29 +27,29 @@ class UNet(nn.Module):
     def __init__(self, in_channels = 1):
         super(UNet, self).__init__()
 
-        self.down1 = down_layer(in_channels, 16)
+        self.down1 = _down_layer(in_channels, 16)
 
-        self.down2 = down_layer(16, 32)
+        self.down2 = _down_layer(16, 32)
 
-        self.down3 = down_layer(32, 64)
+        self.down3 = _down_layer(32, 64)
 
-        self.down4 = down_layer(64, 128)
+        self.down4 = _down_layer(64, 128)
 
-        self.down5 = down_layer(128, 256)
+        self.down5 = _down_layer(128, 256)
 
-        self.down6 = down_layer(256, 512)
+        self.down6 = _down_layer(256, 512)
 
-        self.up1 = up_layer(512, 256, dropout=True)
+        self.up1 = _up_layer(512, 256, dropout=True)
 
-        self.up2 = up_layer(512, 128, dropout=True)
+        self.up2 = _up_layer(512, 128, dropout=True)
 
-        self.up3 = up_layer(256, 64, dropout=True)
+        self.up3 = _up_layer(256, 64, dropout=True)
 
-        self.up4 = up_layer(128, 32)
+        self.up4 = _up_layer(128, 32)
 
-        self.up5 = up_layer(64, 16)
+        self.up5 = _up_layer(64, 16)
 
-        self.up6 = up_layer(32, 1)
+        self.up6 = _up_layer(32, 1)
 
     def forward(self, x):
         x_down1 = self.down1(x)
@@ -87,8 +89,53 @@ class UNet(nn.Module):
 
         return F.sigmoid(x_up6)
 
+class UNetLightning(pl.LightningModule):
+    def __init__(self, in_channels=1, lr=0.0001, weight_decay=0.00001):
+        super(UNetLightning, self).__init__()
+        self.lr = lr
+        self.weight_decay = weight_decay
+
+        self.model = UNet(in_channels)
+        self.loss = nn.L1Loss()
+        self.train_loss = MeanMetric()
+        self.val_loss = MeanMetric()
+
+    def forward(self, x):
+        return self.model(x)
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=self.lr, weight_decay=self.weight_decay)
+
+    def get_loss(self, batch):
+        x, y = batch
+        y_hat = self.forward(x) * x
+        return self.loss(y_hat, y)
+
+    def training_step(self, batch, batch_idx):
+        loss = self.get_loss(batch)
+        self.train_loss.update(loss)
+        return loss
+
+    def on_train_epoch_end(self):
+        train_loss = self.train_loss.compute()
+        self.train_loss.reset()
+        self.log('train_loss', train_loss)
+
+    def validation_step(self, batch, batch_idx):
+        loss = self.get_loss(batch)
+        self.val_loss.update(loss)
+        return loss
+    
+    def on_validation_epoch_end(self):
+        val_loss = self.val_loss.compute()
+        self.val_loss.reset()
+        self.log('val_loss', val_loss)
+
+    def predict_step(self, batch, batch_idx: int, dataloader_idx: int = None):
+        return self.forward(batch)
+
 # Test the model
 if __name__ == '__main__':
-    model = UNet()
+    model = UNetLightning()
     print(model)
     print(model(torch.randn(1, 1, 512, 128)).shape)
