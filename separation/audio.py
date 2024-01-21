aa1a42a6335394b0d37856c8660a5133656d98e3
jljl1337 <lckjack123@gmail.com>
2024-01-21 16:48:10 +0800

    Update load function, separate low level function

--- separation/audio.py
+++ separation/audio.py
@@ -5,11 +5,8 @@ import librosa
 import soundfile as sf
 from scipy.signal.windows import hann
 
-def load(path, offset=0, duration=-1):
-    sr = sf.info(path).samplerate
-    data, sr = sf.read(path, start=int(offset*sr), frames=int(duration*sr),
-                       dtype='float32')
-    return data, sr
+def load(path, sr=None, offset=0, duration=None):
+    return librosa.load(path, sr=sr, offset=offset, duration=duration)
 
 def save(path, data, sample_rate):
     sf.write(path, data, sample_rate)
@@ -21,19 +18,26 @@ def resample(data, source_sr, target_sr):
     data = torchaudio.transforms.Resample(source_sr, target_sr)(data)
     return data
 
-def to_mag_phase(mix_wave, stem_wave, win_length, hop_length):
-    mix_spectrogram = librosa.stft(mix_wave, n_fft=win_length, hop_length=hop_length)
-    stem_spectrogram = librosa.stft(stem_wave, n_fft=win_length, hop_length=hop_length)
-    mix_magnitude, mix_phase = librosa.magphase(mix_spectrogram)
-    stem_magnitude, stem_phase = librosa.magphase(stem_spectrogram)
+def to_mag_phase(wave, win_length, hop_length):
+    spectrogram = librosa.stft(wave, n_fft=win_length, hop_length=hop_length)
+    return librosa.magphase(spectrogram)
+
+# TODO: Too high level?
+def mix_stem_to_mag_phase(mix_wave, stem_wave, win_length, hop_length):
+    mix_magnitude, mix_phase = to_mag_phase(mix_wave, win_length, hop_length)
+    stem_magnitude, stem_phase = to_mag_phase(stem_wave, win_length, hop_length)
 
     mix_mag_max = mix_magnitude.max()
     mix_magnitude /= mix_mag_max
     stem_magnitude /= mix_mag_max
 
-    return mix_magnitude, mix_phase, stem_magnitude, stem_phase
+    return mix_magnitude, mix_phase, stem_magnitude, stem_phase, mix_mag_max
 
-def to_wave(magnitude, phase, mix_spectrogram, win_length, hop_length):
+def to_wave(magnitude, phase, win_length, hop_length):
     spectrogram = magnitude * phase
-    spectrogram *= mix_spectrogram.max()
     return librosa.istft(spectrogram, win_length=win_length, hop_length=hop_length)
+
+# TODO: Too high level?
+def stem_to_wave(magnitude, phase, win_length, hop_length, mag_max):
+    magnitude *= mag_max
+    return to_wave(magnitude, phase, win_length, hop_length)
