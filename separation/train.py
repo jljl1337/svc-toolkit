777d6d18d23458a91de3ad9fd56729cfe9ba6edf
jljl1337 <lckjack123@gmail.com>
2024-01-25 19:15:16 +0800

    Update training

--- separation/train.py
+++ separation/train.py
@@ -1,15 +1,21 @@
 import os
+from argparse import ArgumentParser
+from datetime import datetime
+
 import torch
 import torch.nn as nn
 import numpy as np
 import pandas as pd
+import pytorch_lightning as pl
 from tqdm import tqdm
 from dotenv import load_dotenv
 from torch.utils.data import DataLoader
+from pytorch_lightning.callbacks import EarlyStopping, ModelCheckpoint
 
 import constants
-from data import MusicDataset
-from model import UNet, CombinedLoss
+from data import MagnitudeDataset
+from logger import MyLogger
+from models import UNetLightning
 
 def train(model_list, epoch, loader, combined_loss, optimizer, device,
           model_dir):
@@ -55,6 +61,11 @@ def train(model_list, epoch, loader, combined_loss, optimizer, device,
 
 
 def main():
+    parser = ArgumentParser()
+    parser.add_argument('--train_csv', type=str, default='./train.csv')
+    parser.add_argument('--val_csv', type=str, default='./val.csv')
+    args = parser.parse_args()
+
     load_dotenv()
     TRAIN_DIR = os.getenv('TRAIN_DIR')
     MODEL_DIR = os.getenv('MODEL_DIR')
@@ -62,33 +73,41 @@ def main():
     EPOCHS = int(os.getenv('EPOCHS'))
     LOADER_NUM_WORKERS = int(os.getenv('LOADER_NUM_WORKERS'))
 
-    LEARNING_RATE = constants.LEARNING_RATE
-    WEIGHT_DECAY = constants.WEIGHT_DECAY
+    SAMPLE_RATE = int(os.getenv('SAMPLE_RATE'))
+    WIN_LENGTH = int(os.getenv('WIN_LENGTH'))
+    HOP_LENGTH = int(os.getenv('HOP_LENGTH'))
+    PATCH_LENGTH = int(os.getenv('PATCH_LENGTH'))
+    EXPAND_FACTOR = int(os.getenv('EXPAND_FACTOR'))
 
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    torch.manual_seed(constants.SEED)
-    np.random.seed(constants.SEED)
+    LEARNING_RATE = float(os.getenv('LEARNING_RATE'))
+    WEIGHT_DECAY = float(os.getenv('WEIGHT_DECAY'))
 
-    dataset = MusicDataset(TRAIN_DIR)
-    loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=True,
-                        num_workers=LOADER_NUM_WORKERS, pin_memory=True)
+    pl.seed_everything(constants.SEED, workers=True)
 
-    model_list = nn.ModuleList()
-    for i in range(constants.MODEL_NUM):
-        model_list.append(UNet())
+    dataset_train = MagnitudeDataset(args.train_csv, expand_factor=EXPAND_FACTOR, 
+                                     win_length=WIN_LENGTH, hop_length=HOP_LENGTH,
+                                     patch_length=PATCH_LENGTH)
+    dataset_val = MagnitudeDataset(args.val_csv, expand_factor=EXPAND_FACTOR, 
+                                   win_length=WIN_LENGTH, hop_length=HOP_LENGTH,
+                                   patch_length=PATCH_LENGTH)
 
-    criterion = nn.L1Loss()
-    combined_loss = CombinedLoss(model_list, criterion)
-    optimizer = torch.optim.Adam(combined_loss.parameters(), lr=LEARNING_RATE,
-                                 weight_decay=WEIGHT_DECAY)
+    loader_train = DataLoader(dataset_train, batch_size=BATCH_SIZE, shuffle=True,
+                              num_workers=LOADER_NUM_WORKERS, pin_memory=True)
+    loader_val = DataLoader(dataset_val, batch_size=BATCH_SIZE, shuffle=False,
+                            num_workers=LOADER_NUM_WORKERS, pin_memory=True)
 
-    df = pd.DataFrame(columns=['epoch', 'loss'])
+    now = datetime.now()
+    date_time = now.strftime("%Y%m%d_%H%M%S")
+    save_dir = f'{MODEL_DIR}/{date_time}'
+    os.makedirs(save_dir, exist_ok=True)
 
-    for epoch in range(EPOCHS):
-        loss = train(model_list, epoch, loader, combined_loss, optimizer,
-                     device, MODEL_DIR)
-        df.loc[len(df)] = [epoch, loss]
-        df.to_csv(f'{MODEL_DIR}/loss.csv', index=False)
+    model = UNetLightning(lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY)
+    early_stopping = EarlyStopping(monitor='val_loss', patience=20, mode='min')
+    model_checkpoint = ModelCheckpoint(monitor='val_loss', save_top_k=1, mode='min', save_last=True, dirpath=save_dir)
+    logger = MyLogger(save_dir)
+
+    trainer = pl.Trainer(max_epochs=EPOCHS, callbacks=[early_stopping, model_checkpoint], logger=logger)
+    trainer.fit(model, loader_train, loader_val)
     
 if __name__ == '__main__':
     main()
