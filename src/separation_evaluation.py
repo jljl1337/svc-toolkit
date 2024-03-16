1904c1f35af1b2b053031a97846cacde01149ddf
jljl1337 <lckjack123@gmail.com>
2024-03-16 19:08:21 +0800

    Change to save in model directory instead of new directory

--- src/separation_evaluation.py
+++ src/separation_evaluation.py
@@ -11,8 +11,6 @@ def main():
     parser = ArgumentParser()
     parser.add_argument('-m', '--model_dir', type=str, required=True)
     parser.add_argument('-t', '--test_csv', type=str, required=True)
-    parser.add_argument('-e', '--experiment', type=str, required=True)
-    parser.add_argument('-o', '--output_dir', type=str, default='./evaluation')
     parser.add_argument('-l', '--last', default=False, action=argparse.BooleanOptionalAction)
     args = parser.parse_args()
 
@@ -23,14 +21,11 @@ def main():
 
     df_result.boxplot(grid=False)
 
-    experiment_dir = os.path.join(args.output_dir, args.experiment)
-    os.makedirs(experiment_dir, exist_ok=True)
-
-    plt.savefig(os.path.join(experiment_dir, 'boxplot.png'))
-    df_result.to_csv(os.path.join(experiment_dir, 'result.csv'), index=False)
+    plt.savefig(os.path.join(args.model_dir, f'boxplot{"_last" if args.last else ""}.png'))
+    df_result.to_csv(os.path.join(args.model_dir, f'result{"_last" if args.last else ""}.csv'), index=False)
 
     summary_df = evaluator.summary(df_result)
-    summary_df.to_csv(os.path.join(experiment_dir, 'summary.csv'))
+    summary_df.to_csv(os.path.join(args.model_dir, f'summary{"_last" if args.last else ""}.csv'))
 
 
 if __name__ == '__main__':
