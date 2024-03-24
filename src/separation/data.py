bf9568730689244dc034577fc9838450544ee29d
jljl1337 <lckjack123@gmail.com>
2024-03-24 17:18:18 +0800

    Cleanup, Add data module

--- src/separation/data.py
+++ src/separation/data.py
@@ -5,11 +5,13 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 
 import torch
 import numpy as np
+import pytorch_lightning as pl
 from pandas import read_csv
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
+from pytorch_lightning.trainer.states import TrainerFn
 from tqdm import tqdm
 
-from separation.audio import load, to_magnitude, to_mag_phase, pad_wave
+from separation.audio import load, to_magnitude, to_mag_phase, to_wave, pad_wave
 from separation.constants import CSV_MIXTURE_PATH_COLUMN, CSV_STEM_PATH_COLUMN, NYQUIST, ZERO, NEGLECT_FREQUENCY_OPTIONS
 
 class MagnitudeRandomDataset(Dataset):
@@ -111,7 +113,6 @@ class MagnitudeDataset(Dataset):
     def __init__(
         self,
         paths: Iterable[str],
-        expand_factor: float,
         win_length: int,
         hop_length: int,
         patch_length: int,
@@ -123,27 +124,30 @@ class MagnitudeDataset(Dataset):
             raise ValueError(f'Invalid neglect_frequency: {self.neglect_frequency}')
 
         # Save parameters
+        self.paths = paths
         self.win_length = win_length
         self.hop_length = hop_length
         self.patch_length = patch_length
-        self.expand_factor = expand_factor
         self.neglect_frequency = neglect_frequency
         self.sample_rate = sample_rate
 
+        self._setup()
+
+    def _setup(self):
         # Set up magnitudes
-        self.magnitude_list = [None] * len(paths)
-        self.phase_list = [None] * len(paths)
-        self.magnitude_max_list = [None] * len(paths)
-        self.old_length_list = [None] * len(paths)
+        self.magnitude_list = [None] * len(self.paths)
+        self.phase_list = [None] * len(self.paths)
+        self.magnitude_max_list = [None] * len(self.paths)
+        self.old_length_list = [None] * len(self.paths)
         self.index_channel_patch_list = []
 
         # Load magnitudes in parallel
         with ThreadPoolExecutor(max_workers=cpu_count()) as executor:
             # Submit tasks
-            futures = [executor.submit(self._load_magnitude, index, path) for index, path in enumerate(paths)]
+            futures = [executor.submit(self._load_magnitude, index, path) for index, path in enumerate(self.paths)]
 
             # For each finished task
-            for future in tqdm(as_completed(futures), total=len(paths)):
+            for future in tqdm(as_completed(futures), total=len(self.paths)):
                 index, magnitude, phase, magnitude_max, channel_num, patch_num, old_length = future.result()
 
                 # Save magnitude, phase, and old length
@@ -161,7 +165,9 @@ class MagnitudeDataset(Dataset):
         # Sort index_channel_patch_list
         self.index_channel_patch_list = sorted(self.index_channel_patch_list)
 
-    def _load_magnitude(self, index: int, path: str):
+        self.need_setup = False
+
+    def _load_magnitude(self, index: int, path: str) -> tuple[int, np.ndarray, np.ndarray, float, int, int, int]:
         # Load audio
         wave, _sr = load(path, sr=self.sample_rate, mono=False)
 
@@ -210,6 +216,167 @@ class MagnitudeDataset(Dataset):
         tensor = torch.from_numpy(magnitude)
 
         return tensor
+
+    def get_wave_list(self, mask: np.ndarray, invert=False):
+        if self.need_setup:
+            self._setup()
+
+        if mask.shape != (len(self.index_channel_patch_list), self.win_length / 2, self.patch_length):
+            raise ValueError(f'Invalid mask shape: {mask.shape}')
+
+        # Invert mask if needed
+        if invert:
+            mask = 1 - mask
+
+        max_workers = min(cpu_count(), 4)
+
+        with ThreadPoolExecutor(max_workers=max_workers) as executor:
+            futures = [executor.submit(self._get_song_wave, index, mask) for index in range(len(self.paths))]
+
+            result_list = [future.result() for future in tqdm(as_completed(futures), total=len(self.paths))]
+
+            self.index_channel_patch_list = None
+            self.need_setup = True
+
+            return sorted(result_list)
+
+    def _get_song_wave(self, index: int, mask: np.ndarray):
+        for i in (range(len(self.index_channel_patch_list))):
+            actual_index, channel, patch = self.index_channel_patch_list[i]
+            
+            if index != actual_index:
+                continue
+            
+            start = patch * self.patch_length
+            end = start + self.patch_length
+
+            if self.neglect_frequency == NYQUIST:
+                # result_magnitude[index][channel, :-1, start: end] = mask[i]
+                self.magnitude_list[index][channel, :-1, start: end] = mask[i]
+            elif self.neglect_frequency == ZERO:
+                # result_magnitude[index][channel, 1:, start: end] = mask[i]
+                self.magnitude_list[index][channel, 1:, start: end] = mask[i]
+
+        self.magnitude_list[index] *= self.magnitude_max_list[index]
+
+        # wave = to_wave(result_magnitude[i], self.phase_list[i], self.win_length, self.hop_length)
+        wave = to_wave(self.magnitude_list[index], self.phase_list[index], self.win_length, self.hop_length)
+
+        old_length = self.old_length_list[index]
+        wave = wave[:, :old_length]
+
+        self.magnitude_list[index] = None
+        self.phase_list[index] = None
+        self.magnitude_max_list[index] = None
+        self.old_length_list[index] = None
+
+        return index, wave
+
+class MagnitudeDataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        train_csv: str = None,
+        val_csv: str = None,
+        predict_path_list: Iterable[str] = None,
+        expand_factor: float = None,
+        win_length: int = None,
+        hop_length: int = None,
+        patch_length: int = None,
+        neglect_frequency: str = None,
+        sample_rate: int = None,
+        batch_size: int = None,
+        loader_num_workers: int = None,
+    ):
+        super().__init__()
+
+        self.train_csv = train_csv
+        self.val_csv = val_csv
+        self.predict_path_list = predict_path_list
+        self.expand_factor = expand_factor
+        self.win_length = win_length
+        self.hop_length = hop_length
+        self.patch_length = patch_length
+        self.neglect_frequency = neglect_frequency
+        self.sample_rate = sample_rate
+        self.batch_size = batch_size
+        self.loader_num_workers = loader_num_workers
+
+        self.train_dataset = None
+        self.val_dataset = None
+        self.predict_dataset = None
+
+        self.dataset_kwargs = {
+            'expand_factor': expand_factor,
+            'win_length': win_length,
+            'hop_length': hop_length,
+            'patch_length': patch_length,
+            'neglect_frequency': neglect_frequency,
+            'sample_rate': sample_rate
+        }
+
+    def setup(self, stage: str = None):
+        if stage == TrainerFn.FITTING and self.train_dataset is None and self.val_dataset is None:
+            print('Loading training datasets')
+            self.train_dataset = MagnitudeRandomDataset(
+                self.train_csv,
+                **self._dataset_kwargs(fit=True)
+            )
+
+            print('Loading validation datasets')
+            self.val_dataset = MagnitudeRandomDataset(
+                self.val_csv,
+                **self._dataset_kwargs(fit=True)
+            )
+
+        elif stage == TrainerFn.PREDICTING and self.predict_dataset is None:
+            self.predict_dataset = MagnitudeDataset(
+                self.predict_path_list,
+                **self._dataset_kwargs(fit=False)
+            )
+
+    def _dataset_kwargs(self, fit: bool):
+        kwargs_dict = {
+            'win_length': self.win_length,
+            'hop_length': self.hop_length,
+            'patch_length': self.patch_length,
+            'neglect_frequency': self.neglect_frequency,
+            'sample_rate': self.sample_rate
+        }
+
+        if fit:
+            kwargs_dict['expand_factor'] = self.expand_factor
+
+        return kwargs_dict
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_dataset,
+            **self._loader_kwargs(shuffle=True),
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_dataset,
+            **self._loader_kwargs(shuffle=False),
+        )
+
+    def predict_dataloader(self):
+        return DataLoader(
+            self.predict_dataset,
+            **self._loader_kwargs(shuffle=False),
+        )
+
+    def _loader_kwargs(self, shuffle: bool):
+        return {
+            'batch_size': self.batch_size,
+            'num_workers': self.loader_num_workers,
+            'persistent_workers': True,
+            'pin_memory': True,
+            'shuffle': shuffle,
+        }
+
+    def get_wave_list(self, mask: np.ndarray, invert=False):
+        self.predict_dataset.get_wave_list(mask, invert)
 
 if __name__ == "__main__":
     print('test')
