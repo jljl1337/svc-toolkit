07a4d793b47a340b17726bef18bec08bad0f585b
jljl1337 <lckjack123@gmail.com>
2024-04-02 17:35:56 +0800

    Add test for get_available_device function

---
+++ tests/presenter/test_common.py
@@ -0,0 +1,6 @@
+from presenter.common import get_available_device
+
+def test_get_available_device():
+    devices = get_available_device()
+    assert len(devices) > 0
+    assert devices[-1] == ('CPU', 'cpu')
