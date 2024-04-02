3191a7d3fb2350dd0112038191376cf525986712
jljl1337 <lckjack123@gmail.com>
2024-04-02 17:13:45 +0800

    Update test for separation presenter

--- tests/presenter/test_separation.py
+++ tests/presenter/test_separation.py
@@ -1,7 +1,10 @@
+import os
+import shutil
+
 from presenter.separation import SeparationPresenter
 
 # Create a VocalSeparationWidget dummy class
-class VocalSeparationWidgetDummy:
+class SeparationWidgetDummy:
     def set_model_list(self, model_list):
         self.model_list = model_list
 
@@ -18,71 +21,178 @@ class SeparatorDummy:
         self.output_path = output_path
         self.invert = invert
         self.emit = emit
-    
+
 # Create a SeparatorFactory dummy class
 class SeparatorFactoryDummy:
     separator = SeparatorDummy()
 
-    def create(self, model, device):
-        self.model = model
+    def create(self, model_dir, device, precision):
+        self.model_dir = model_dir
         self.device = device
+        self.precision = precision
 
         return self.separator
 
+def test_separation_presenter_get_manifest():
+    # Create a VocalSeparationWidget dummy object
+    view = SeparationWidgetDummy()
+    # Create a SeparatorFactory dummy object
+    model_factory = SeparatorFactoryDummy()
+    # Create a VocalSeparationPresenter object
+    presenter = SeparationPresenter(view, model_factory)
+
+    # Check if can load the manifest
+    assert 'models' in presenter._get_manifest()
+
+def test_separation_presenter_get_model_list():
+    # Create a VocalSeparationWidget dummy object
+    view = SeparationWidgetDummy()
+    # Create a SeparatorFactory dummy object
+    model_factory = SeparatorFactoryDummy()
+    # Create a VocalSeparationPresenter object
+    presenter = SeparationPresenter(view, model_factory)
+
+    # Check if number of keys in the manifest is equal to the number of models
+    dummy_manifest = {'models': {'model1': {}, 'model2': {}}}
+    assert len(presenter._get_model_list(dummy_manifest)) == len(dummy_manifest['models'])
+
 # Test for the VocalSeparationPresenter class constructor with dummy class
-def test_vocal_separation_presenter_constructor():
-    vocal_separation_widget = VocalSeparationWidgetDummy()
-    separator_factory = SeparatorFactoryDummy()
-    vocal_separation_presenter = SeparationPresenter(vocal_separation_widget, separator_factory)
+def test_separation_presenter_constructor():
+    # Create a VocalSeparationWidget dummy object
+    view = SeparationWidgetDummy()
+    # Create a SeparatorFactory dummy object
+    model_factory = SeparatorFactoryDummy()
+    # Create a VocalSeparationPresenter object
+    presenter = SeparationPresenter(view, model_factory)
 
-    assert vocal_separation_widget.model_list == vocal_separation_presenter._get_model_list()
-    assert vocal_separation_widget.device_list[-1] == ('CPU', 'cpu')
-    assert vocal_separation_widget.separation_function == vocal_separation_presenter.start_separation
+    # Check if the model list is set
+    assert len(view.model_list) > 0
+    # Check if the device list is set
+    assert view.device_list[-1] == ('CPU', 'cpu')
+    # Check if the separation function is set
+    assert view.separation_function == presenter.start_separation
+    # Check if the model factory is set
+    assert presenter.model_factory == model_factory
+    # Check if the manifest is set
+    assert type(presenter.manifest) == dict
 
-# Test for start_separation method with vocal only
+def test_separation_presenter_get_model_dir():
+    # Create a VocalSeparationWidget dummy object
+    view = SeparationWidgetDummy()
+    # Create a SeparatorFactory dummy object
+    model_factory = SeparatorFactoryDummy()
+    # Create a VocalSeparationPresenter object
+    presenter = SeparationPresenter(view, model_factory)
+
+    # Create a dummy model
+    model = 'dummy_model'
+    # Set the manifest
+    presenter.manifest = {'models': {'dummy_model': {'subfolder': 'dummy_subfolder'}}}
+
+    # Check if the model directory is set correctly
+    assert presenter._get_model_dir(model).endswith('dummy_subfolder')
+
+def test_separation_presenter_download_model_if_needed():
+    # Create a VocalSeparationWidget dummy object
+    view = SeparationWidgetDummy()
+    # Create a SeparatorFactory dummy object
+    model_factory = SeparatorFactoryDummy()
+    # Create a VocalSeparationPresenter object
+    presenter = SeparationPresenter(view, model_factory)
+
+    # Create a dummy model
+    model = 'Test Model'
+    # Create a dummy model directory
+    model_dir = os.path.join(os.path.dirname(__file__), 'test_model')
+    # Set the manifest
+    presenter.manifest = {
+        'models': {
+            'Test Model': {
+                'repo_id': 'jljl1337/singing-voice-conversion-gui-test',
+                'subfolder': 'test_model',
+                'assets': {
+                    'model': 'model.tmp',
+                    'config': 'config.tmp'
+                }
+            }
+        }
+    }
+
+    # Remove the model directory if it exists
+    if os.path.exists(model_dir):
+        shutil.rmtree(model_dir)
+
+    # Call the download_model_if_needed function
+    presenter._download_model_if_needed(model, model_dir)
+
+    # Check if the model directory is created
+    assert os.path.exists(model_dir)
+    # Check if the files are downloaded
+    assert os.path.exists(os.path.join(model_dir, 'model.tmp'))
+    assert os.path.exists(os.path.join(model_dir, 'config.tmp'))
+
+    # Get model file last modified time
+    model_file_time = os.path.getmtime(os.path.join(model_dir, 'model.tmp'))
+
+    # Call again the download_model_if_needed function
+    presenter._download_model_if_needed(model, model_dir)
+
+    # Check if the model file is not downloaded again
+    assert model_file_time == os.path.getmtime(os.path.join(model_dir, 'model.tmp'))
+
+    # Remove the model directory
+    shutil.rmtree(model_dir)
+    
 def test_vocal_separation_presenter_start_separation():
-    vocal_separation_widget = VocalSeparationWidgetDummy()
-    separator_factory = SeparatorFactoryDummy()
-    vocal_separation_presenter = SeparationPresenter(vocal_separation_widget, separator_factory)
-    vocal_separation_presenter.start_separation(lambda x: x, 'file', 'output_dir', True, False, 'model', 'device')
+    # Create a VocalSeparationWidget dummy object
+    view = SeparationWidgetDummy()
+    # Create a SeparatorFactory dummy object
+    model_factory = SeparatorFactoryDummy()
+    # Create a VocalSeparationPresenter object
+    presenter = SeparationPresenter(view, model_factory)
 
-    assert separator_factory.model == 'model'
-    assert separator_factory.device == 'device'
+    # Create a dummy emit function
+    def emit(value):
+        return value
 
-    assert separator_factory.separator.file == 'file'
-    assert separator_factory.separator.output_path == 'output_dir/vocal.wav'
-    assert separator_factory.separator.invert == False
-    assert separator_factory.separator.emit(100) == 100
-    assert separator_factory.separator.emit(50) == 50
+    # Create a dummy file
+    file = 'dummy_file'
+    # Create a dummy output directory
+    output_dir = 'dummy_output_dir'
+    # Create a dummy vocal flag
+    vocal = True
+    # Create a dummy non-vocal flag
+    non_vocal = True
+    # Create a dummy model
+    model = 'Test'
+    # Create a dummy device
+    device = 'dummy_device'
+    # Create a dummy precision
+    precision = 'dummy_precision'
+    # Set the manifest
+    presenter.manifest = {
+        'models': {
+            'Test': {
+                'repo_id': 'jljl1337/singing-voice-conversion-gui-test',
+                'subfolder': 'test_model',
+                'assets': {
+                    'model': 'model.tmp',
+                    'config': 'config.tmp'
+                }
+            }
+        }
+    }
 
-# Test for start_separation method with non vocal only
-def test_vocal_separation_presenter_start_separation_non_vocal():
-    vocal_separation_widget = VocalSeparationWidgetDummy()
-    separator_factory = SeparatorFactoryDummy()
-    vocal_separation_presenter = SeparationPresenter(vocal_separation_widget, separator_factory)
-    vocal_separation_presenter.start_separation(lambda x: x, 'file', 'output_dir', False, True, 'model', 'device')
+    # Call the start_separation function
+    presenter.start_separation(emit, file, output_dir, vocal, non_vocal, model, device, precision)
 
-    assert separator_factory.model == 'model'
-    assert separator_factory.device == 'device'
+    # Check if the model directory is set correctly
+    assert model_factory.model_dir.endswith('test_model')
+    # Check if the device is set correctly
+    assert model_factory.device == device
+    # Check if the file is set correctly
+    assert model_factory.separator.file == file
 
-    assert separator_factory.separator.file == 'file'
-    assert separator_factory.separator.output_path == 'output_dir/instrument.wav'
-    assert separator_factory.separator.invert == True
-    assert separator_factory.separator.emit(100) == 100
-    assert separator_factory.separator.emit(50) == 50
-
-# Test for start_separation method with vocal and non vocal
-def test_vocal_separation_presenter_start_separation_vocal_non_vocal():
-    vocal_separation_widget = VocalSeparationWidgetDummy()
-    separator_factory = SeparatorFactoryDummy()
-    vocal_separation_presenter = SeparationPresenter(vocal_separation_widget, separator_factory)
-    vocal_separation_presenter.start_separation(lambda x: x, 'file', 'output_dir', True, True, 'model', 'device')
-
-    assert separator_factory.model == 'model'
-    assert separator_factory.device == 'device'
-
-    assert separator_factory.separator.file == 'file'
-    assert separator_factory.separator.output_path == 'output_dir/instrument.wav'
-    assert separator_factory.separator.invert == True
-    assert separator_factory.separator.emit(100) == 100
-    assert separator_factory.separator.emit(50) == 75
+    # Remove the model directory
+    print(os.path.dirname(__file__))
+    shutil.rmtree(os.path.join(os.path.dirname(__file__), '../../models', model_factory.model_dir))
