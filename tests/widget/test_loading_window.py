4515cba4f47d7369b9121ab3faafada9ab55f414
jljl1337 <lckjack123@gmail.com>
2024-04-03 03:59:52 +0800

    Add test for LoadingWindow class

---
+++ tests/widget/test_loading_window.py
@@ -0,0 +1,7 @@
+from widget.loading_window import LoadingWindow
+
+def test_loading_window(qtbot):
+    loading_window = LoadingWindow()
+    qtbot.addWidget(loading_window)
+
+    assert loading_window.windowTitle() == 'Loading...'
