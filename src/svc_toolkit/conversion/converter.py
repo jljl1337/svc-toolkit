48b3a81b62b70547e279acfe289aa1600a9ca61a
jljl1337 <lckjack123@gmail.com>
2024-04-11 10:23:16 +0800

    Add soundfile and librosa dependencies for audio processing

--- src/svc_toolkit/conversion/converter.py
+++ src/svc_toolkit/conversion/converter.py
@@ -1,3 +1,7 @@
+import numpy as np
+import soundfile as sf
+from librosa import load
+
 from so_vits_svc_fork.inference.main import infer
 
 class ConverterFactory:
@@ -23,3 +27,21 @@ class Converter:
             device=self.device,
             **kwargs
         )
+
+        input_wave, _input_sr = load(input_path, mono=False, sr=None)
+
+        # Get number of channels
+        num_channels = input_wave.shape[0]
+
+        # Load the output file
+        output_wave, _output_sr = load(output_path, mono=False, sr=None)
+
+        # Set a ndarray with zeros to the same shape as the input
+        new_output_wave = np.zeros_like(input_wave)
+
+        # Save the output file with the same number of channels
+        for i in range(num_channels):
+            new_output_wave[i] = output_wave[i]
+
+        # Save the output file with the same number of channels
+        sf.write(output_path, new_output_wave.T, _output_sr)
